2da91d9436854f3cbd8583ce67a4814bc4b795d4
EnanaAtomica <zcabrera@lasalleflorida.edu.ar>
2024-08-01 12:10:40 -0300

    a

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
@@ -20,7 +21,7 @@ app.use(express.json());
 // Ruta de ejemplo para obtener datos
 app.get('/data', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM your_table_name');
+    const result = await pool.query('SELECT * FROM ');
     res.json(result.rows);
   } catch (error) {
     console.error('Error ejecutando la consulta', error);
@@ -32,3 +33,17 @@ app.get('/data', async (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
+*/
+
+const pgp = require('pg-promise')(/* options */)
+// const db = pgp('postgresql://base_de_datos_1erintento_user:c1hAhfjFt87HP0tGev3NpqZJK2wbCURW@dpg-cqlmvglumphs7396v9a0-a/base_de_datos_1erintento')
+const db = pgp('postgres://base_de_datos_1erintento_user:c1hAhfjFt87HP0tGev3NpqZJK2wbCURW@dpg-cqlmvglumphs7396v9a0-a:5432/base_de_datos_1erintento')
+
+
+db.one('SELECT * AS value')
+  .then((data) => {
+    console.log('DATA:', data.value)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
