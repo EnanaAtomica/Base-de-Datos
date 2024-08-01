5e864eac29fa15d41af5e8e446167ff3117ebfd3
EnanaAtomica <zcabrera@lasalleflorida.edu.ar>
2024-08-01 10:08:11 -0300

    files a utilizar y config de BdD

---
+++ configBD.js
@@ -0,0 +1,8 @@
+module.exports = {
+    DB_NAME: process.env.DB_NAME || 'base_de_datos_1erintento',
+    DB_USER: process.env.DB_USER || 'base_de_datos_1erintento_user',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'c1hAhfjFt87HP0tGev3NpqZJK2wbCURW',
+    DB_HOST: process.env.DB_HOST || 'postgresql://base_de_datos_1erintento_user:c1hAhfjFt87HP0tGev3NpqZJK2wbCURW@dpg-cqlmvglumphs7396v9a0-a/base_de_datos_1erintento',  // Utiliza la URL interna proporcionada por Render
+    DB_PORT: process.env.DB_PORT || 5432
+  };
+  
